e0d37c022a73a4c725546b29df06780ac5bd53e3
Harshdeep Singh <harshdeepsinghofficial2005@gmail.com>
2021-06-12 18:29:30 +0530

    Added templates

--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,9 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def blogHome(request):
-    return HttpResponse('This is blogHome')
+    return render(request, 'blog/blogHome.html')
 
 
 def blogPost(request, slug):
-    return HttpResponse(f'This is blogPost: {slug}')
+    return render(request, 'blog/blogPost.html')
--- home/views.py
+++ home/views.py
@@ -1,14 +1,13 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('This is home')
+    return render(request, 'home/home.html')
 
 
 def about(request):
-    return HttpResponse('This is about')
+    return render(request, 'home/about.html')
 
 
 def contact(request):
-    return HttpResponse('This is contact')
+    return render(request, 'home/contact.html')
