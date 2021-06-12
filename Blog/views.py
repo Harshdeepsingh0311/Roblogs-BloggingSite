f3aff09fb160e05c87986535445598135e4b6f0c
Harshdeep Singh <harshdeepsinghofficial2005@gmail.com>
2021-06-12 10:54:59 +0530

    Added new app home and built the basic url pipeline

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse('This is blogHome')
+
+
+def blogPost(request, slug):
+    return HttpResponse(f'This is blogPost: {slug}')
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('This is home')
+
+
+def about(request):
+    return HttpResponse('This is about')
+
+
+def contact(request):
+    return HttpResponse('This is contact')
