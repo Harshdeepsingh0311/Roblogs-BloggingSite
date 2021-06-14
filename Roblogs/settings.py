0b8708ee3d8da4fd43c029918fb45c7b3f5eb29f
Harshdeep Singh <harshdeepsinghofficial2005@gmail.com>
2021-06-14 20:28:10 +0530

    Designed Replies

--- Roblogs/settings.py
+++ Roblogs/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q#(w4w+9f32#&4g(%_n2!q@#nyt8^7k-=0io$s6nwz=#uu(njt'
+SECRET_KEY = 'Sorry, Enter Your Own Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
